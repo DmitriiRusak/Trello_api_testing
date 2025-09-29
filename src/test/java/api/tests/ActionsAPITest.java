17264208d36eefd8a9b4c8f947746f082c8cfff6
Dmitrii <dimarusak5@gmail.com>
2025-09-29 09:34:14 +0300

    change configuration of a project to run all tests via ci_2

--- src/test/java/api/tests/ActionsAPITest.java
+++ src/test/java/api/tests/ActionsAPITest.java
@@ -103,7 +103,7 @@ public class ActionsAPITest {
         String boardNameRecivedFromApiCall = response.jsonPath().getString("name");
 
         Assert.assertEquals(response.getStatusCode(), 200);
-        Assert.assertEquals(boardNameRecivedFromApiCall, configTestData.BOARD_NAME);
+        Assert.assertEquals(boardNameRecivedFromApiCall, configTestData.BOARD_NAME + "mistake");
     }
 
     @Test(priority = 4)
